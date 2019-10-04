37ede4de690c5959cdaf0d9162da18e13ea70e18
Noor Dawod <noor@fine47.com>
2019-10-04 18:50:06 +0200

    Ensure that the callback is called only once.

--- lib/after_layout.dart
+++ lib/after_layout.dart
@@ -3,11 +3,17 @@ library after_layout;
 import 'package:flutter/widgets.dart';
 
 mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
+  bool _hasBeenCalled;
+
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) => afterFirstLayout(context));
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if(true != _hasBeenCalled) {
+        _hasBeenCalled = true;
+        afterFirstLayout(context);
+      }
+    });
   }
 
   void afterFirstLayout(BuildContext context);
